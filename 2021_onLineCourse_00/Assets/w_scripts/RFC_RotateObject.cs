e4e2f98f5c3ce3868e9a3332bf22bd3b50657648
bosechristensen <74532257+bosechristensen@users.noreply.github.com>
2021-05-23 23:13:18 +0800

    finally only decent

---
+++ 2021_onLineCourse_00/Assets/w_scripts/RFC_RotateObject.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//使用UI一定要宣告
+
+
+public class RFC_RotateObject : MonoBehaviour
+{
+    public GameObject UI_Canvas;
+    public Text Text_RayHitObject;
+
+    //射線功能的起手勢
+    Ray ray;
+    RaycastHit raycastHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RFC_UI_Test();
+        RFC_RotateTargetObject();
+    }
+    void RFC_UI_Test()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);//定義由攝影機發出到滑鼠位置的射線
+        if (Physics.Raycast(ray, out raycastHit)) //定義射線打中物件之代理變數raycastHit
+        {
+            //顯示射線
+            //Debug.DrawLine(Camera.main.transform.position, raycastHit.transform.position, Color.red, 0.5f, true);//只能顯示射線為擊中物件中心
+            Debug.DrawLine(Camera.main.transform.position, raycastHit.point, Color.red, 0.5f, true);//任意顯示擊中點
+            //起點, 擊中點位置, 顯示顏色, 持續時間, 深度測試
+
+            if (raycastHit.transform.gameObject.name == gameObject.name)//如果接受射線者名稱就是本體物件名稱時
+            {
+                UI_Canvas.SetActive(true);
+                //UI.text
+                Text_RayHitObject.text = $"<color=green>The name of the object hit by Ray: </color> {raycastHit.transform.gameObject.name}";
+
+            }
+            else
+            {
+                UI_Canvas.SetActive(false);
+            }
+        }
+    }
+    void RFC_RotateTargetObject()//按下滑鼠左鍵連續發出Ray打中目標Object即旋轉
+    {
+        //定義射線如何生成
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);//定義由攝影機發出到滑鼠位置的射線
+        if (Physics.Raycast(ray, out raycastHit))//Physics.Raycast()方法檢測射線碰撞資訊(要碰撞客體物件該物件必須有(Collider))
+        {
+            if (Input.GetMouseButton(0) && raycastHit.transform.gameObject.name == gameObject.name)//滑鼠左鍵按下且未鬆開時回傳 True否則為 False
+            {
+                Vector3 RotateVector = new Vector3(0, 1, 0);
+                transform.Rotate(RotateVector);
+            }
+        }
+    }
+
+
+}
---
+++ 2021_onLineCourse_00/Assets/w_scripts/RayFromObject_LightDetector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayFromObject_LightDetector : MonoBehaviour
+{
+    public GameObject SensorSpotLight;
+    public float RayDistance = 3.0f; //顯示射線距離
+
+    //射線功能的起手勢
+    Ray ray;
+    RaycastHit raycastHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayFromObject_Detector();
+    }
+
+    void RayFromObject_Detector()
+    {
+        //生成射線
+        ray = new Ray(transform.position, transform.forward);//注意射線方向是前方(物件藍色箭頭向)
+        //顯示射線
+        Debug.DrawRay(ray.origin, ray.direction * RayDistance, Color.green);//duration和depthTest採預設值
+
+        if (Physics.Raycast(ray, out raycastHit))//一定要使用if程序才能使用raycastHit的各種子函式得到回傳資訊
+        {
+            //印出 raycastHit 被打到的collider相關資訊
+            //print($"The Object's name hit by ray: {raycastHit.transform.gameObject.name}");
+            //print($"{raycastHit.transform.gameObject.name}; {raycastHit.collider.gameObject.name}"); //兩種印出物件名稱的方式raycastHit函式方法要抓到gameobject, 
+            //需使用raycastHit.transform或者raycastHit.collider
+            //print($"The distance from hit object:{raycastHit.distance}");
+            //print($"The point of the ray projecting on the object: {raycastHit.point}");
+            if (raycastHit.transform.gameObject.name == "MainCharacter")
+            {    
+                print($"The Object's name hit by ray: {raycastHit.transform.gameObject.name}");
+                SensorSpotLight.SetActive(true);
+            }
+        }
+    }
+
+}
