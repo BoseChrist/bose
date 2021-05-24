873be4910e1622fd12340fce390e522a4e60ffe2
bosechristensen <74532257+bosechristensen@users.noreply.github.com>
2021-05-24 14:35:37 +0800

    updated llift

---
+++ Inception version3/Assets/ControllerXR.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControllerXR : MonoBehaviour
+{
+
+    public XRNode inputSource;
+    private XRRig rig;
+    private Quaternion rotation;
+
+    void Start()
+    {
+        rig = GetComponent<XRRig>();
+    }
+
+    void Update()
+    {
+        device.TryGetFeatureValue(CommonUsages.deviceRotation, out rotation);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 roll = rotation.eulerAngles;
+        print("roll " + roll.x);
+    }
+
+
+
+
+}
